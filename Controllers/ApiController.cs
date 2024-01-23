0d840862cc69442f29804e429c5325ba88939547
shu0shuo <155411561+shu0shuo@users.noreply.github.com>
2024-01-23 13:11:08 +0800

    return createCarouselItems(file.img.text, file.img["@description"])

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -15,6 +15,10 @@ namespace WebApplication2.Controllers
 
         public IActionResult Index()
         {
+            return Content("<h2>Content, 你好</h2>", "text/plain", System.Text.Encoding.UTF8);
+        }
+        public IActionResult First()
+        {
             return View();
         }
 
@@ -22,6 +26,20 @@ namespace WebApplication2.Controllers
         {
            var cities = _dbContext.Addresses.Select(a => a.City).Distinct();
             return Json(cities);
+
+        }
+
+        public IActionResult Avatar(int id=1)
+        {
+            Member? member =_dbContext.Members.Find(id);
+            if (member != null) 
+            {
+                byte[] img = member.FileData;
+                return File(img, "image/jpeg");
+            }
+
+            return NotFound();
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WebApplication2
 ¹Ï¤ùºé¨ú«Ý­×´_
+ return createCarouselItems(file.img.text, file.img["@description"])«Ý­×´_
