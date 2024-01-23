858befecef0917a8c82ddfbbd4cd51d0362bf836
shu0shuo <155411561+shu0shuo@users.noreply.github.com>
2024-01-23 09:22:34 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ApiController : Controller
+    {
+        //注入DBContext物件--------------
+        private readonly MyDBContext _dbContext;
+        public ApiController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        //-----------------------------------
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+           var cities = _dbContext.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //依賴注入
+            //Configuration: appsettings.json/app.config, GetConnectionString:裡面的連線字串，可有多組
+            builder.Services.AddDbContext<MyDBContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MyDBConnection")
+));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
